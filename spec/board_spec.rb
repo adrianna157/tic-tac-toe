0bdea74738e163c2157605c0ccabbc5d64988ca7
Adrianna Guevarra <guevarra@oregonstate.edu>
2020-06-09 10:32:14 -0700

    create/fail board_spec method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,9 +1,27 @@
 require_relative '../lib/board'
 
 describe 'A Tic Tac Toe board' do
+ let(:board) {Board.new}
+
 
   it 'needs covered with tests' do
     board = Board.new
   end
+
+  # it 'has a place' do
+  #     board = Board.new
+  #     @token = stub(:token)
+  #     @row = stub(:row)
+  #     @column = stub(:column)
+  
+  #   #expect(board.place(1,2,3)).to equal(1) 
+  #    expect board.place(token, row, column).to eq(nil)
+  # end
+  
+  
+
+
+
+
 
 end
